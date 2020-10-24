09663db339027b321898958992757550dca85001
vero.henao <vero14951@gmail.com>
2020-10-24 11:34:36 -0500

    Creacion del proyecto

---
+++ src/main/java/com/videogamerent/app/VideoGameRentApplication.java
@@ -0,0 +1,13 @@
+package com.videogamerent.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameRentApplication.class, args);
+	}
+
+}
